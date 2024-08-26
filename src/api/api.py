89325fadbc508d0c57b687bd8e0f93cba68a1ff7
HuyDinh <huydinh061200@gmail.com>
2024-08-26 22:20:24 +0700

    add 2 summary methods for 2 models

--- src/api/api.py
+++ src/api/api.py
@@ -2,22 +2,33 @@ from fastapi import FastAPI
 from pydantic import BaseModel 
 
 # from models.predict_model import a  ## can run from inside src/
-# from src.models.predict_model import a  ## can run from outside src/
+from src.models.predict_model import t5_summary, bart_summary  ## can run from outside src/
 
 
 app = FastAPI(debug=True) 
 
 class TextRequest(BaseModel): 
+    "define a components of an API"
+    
     text: str 
-
+    model: str
+    lang: str
 
 @app.post("/summarization_text") 
-def process_text(request: TextRequest): 
+def summarization_text(request: TextRequest): 
+    # summary with model t5
+    if 't5' in request.model.lower():
+        summary = t5_summary(text=request.text, lang=request.lang)
 
-    result = request.text
-    return result
+    # summary with model bart
+    elif 'bart' in request.model.lower():
+        summary = bart_summary(text=request.text, lang=request.lang)
+
+    return {'summary': summary}
+
 
 
 if __name__ == "__main__": 
     import uvicorn 
     uvicorn.run(app, host="0.0.0.0", port=8000)
+    # fastapi dev api.py
