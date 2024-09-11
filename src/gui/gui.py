7922b8e89640e5aa1fceb8b51e0af9ac20b6c065
HuyDinh <huydinh061200@gmail.com>
2024-09-11 18:17:14 +0700

    merge gui, add read pdf and doc file

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -7,44 +7,60 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../.
 
 
 from src.settings import FASTAPI_PORT
+from src.features.process_file import read_text, read_pdf, read_doc
 
 st.set_page_config(layout='wide')
 
 st.title("Ứng dụng tóm tắt tóm lược đơn văn bản",)
 
 input_type = st.radio("Lựa chọn kiểu đầu vào", ["Nhập trực tiếp", "Tải file lên"], index=0)
+
+# Input text
 if input_type == "Nhập trực tiếp":
-    input = st.text_area("Nhập văn bản để tóm tắt tóm lược:", height=250)
+    text_content = st.text_area("Nhập văn bản để tóm tắt tóm lược:", height=250)
+# Input file 
 elif input_type == "Tải file lên":
-    input = st.file_uploader("Chọn tệp văn bản để tải lên", type=["txt", "pdf", "docx", 'doc'])
-    if input:
-        file_type = input.name.split(".")[-1]
+    upload_file = st.file_uploader("Chọn tệp văn bản để tải lên", type=["txt", "pdf", "docx", "doc"])
+    if upload_file:
+        file_type = upload_file.name.split(".")[-1]
+        # Read txt file
         if file_type == 'txt':
-            input = input.read().decode("utf-8")
-            st.write(input)
+            text_file = read_text(upload_file)
+        # Read pdf file 
+        elif file_type == 'pdf':
+            text_file = read_pdf(upload_file)
+        # Read docx, doc file 
+        elif 'doc' in file_type:
+            text_file = read_doc(upload_file)
+        # Get text content to send to API
+        text_content = text_file
     else:
         st.write("Chưa chọn file nào")
-        # doc = Document(uploaded_file)
-        # text_input = "\n".join([para.text for para in doc.paragraphs])
 
 
 response = None
 
+
 col1, col2, col3 = st.columns((3, 8, 2))
 
 with col1:
-    level1 = st.radio("Lựa chọn ngôn ngữ", ["Tiếng Việt", "Tiếng Anh"], index=0)
+    language = st.radio("Lựa chọn ngôn ngữ", ["Tiếng Việt", "Tiếng Anh"], index=0)
 
 with col2:
-    level2 = st.radio("Lựa chọn mô hình",
+    model_type = st.radio("Lựa chọn mô hình",
                         ["Mô hình T5", "Mô hình BART"],
                         index=0)
 
 
 
-if col3.button("Summary", use_container_width=True):
+if col3.button("Tóm tắt", use_container_width=True):
+
+        
     response = requests.post(f"http://localhost:{FASTAPI_PORT}/summarization_text",
-                             json={"text": input, "model": level2, "lang": level1})
+                             json={"text": text_content, 
+                                   "model": model_type, 
+                                   "lang": language,
+                                   })
 
 if response:
     result = response.json()
